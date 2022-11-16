f983f1228c0489a4c52df935e0a18b3d6315ff62
David Uhlmann <dib542@gmail.com>
2022-11-16 13:57:52 -0500

    Abstract out tests start function into its own script

---
+++ testnet/scripts/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+# wait a bit for the leader to start the chain
+sleep 10;
+
+# start chain and become a node
+KEEP_RUNNING=false sh ./scripts/startup.sh
+
+# wait for a block or so
+sleep 10;
+
+# run tests for this node
+bash /root/.duality/scripts/run_tests_of_indexes.sh;
+
+# keep container running
+tail -f /dev/null;
